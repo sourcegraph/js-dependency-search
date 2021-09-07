e47e6312ed6c2ba70693e03980b9a4735be9b03b
TJ Kandala <kandalatj@gmail.com>
2021-09-07 07:43:08 -0400

    modernize extension API usage

--- src/js-dependency-search.ts
+++ src/js-dependency-search.ts
@@ -1,71 +1,82 @@
-import { EMPTY, from, of } from 'rxjs';
-import { concatMap, toArray } from 'rxjs/operators';
-import * as sourcegraph from 'sourcegraph'
+import { EMPTY, from, of } from "rxjs";
+import { concatMap, toArray } from "rxjs/operators";
+import * as sourcegraph from "sourcegraph";
+
+const jsDependencySearchDecorationType = sourcegraph.app.createDecorationType();
 
 export function activate(): void {
-   sourcegraph.search.registerQueryTransformer({
-       transformQuery: (query: string) => {
-           const jsImportsRegex = /\bjs.depends:([^\s]*)/
-           if (query.match(jsImportsRegex)) {
-               const jsImportsFilter = query.match(jsImportsRegex)
-               const jsPkg = jsImportsFilter && jsImportsFilter.length >= 1 ? jsImportsFilter[1] : ''
-               const es6Import = '^import.*\''+jsPkg+'[/?^\\s]*\''
-               const commonJSImport = 'require\\(\'' +jsPkg+'[^\\s]*\'\\)'
-               return query.replace(jsImportsRegex  , `(${es6Import}|${commonJSImport})`)
-           }
-           return query
-        }
-   })
+    sourcegraph.search.registerQueryTransformer({
+        transformQuery: (query: string) => {
+            const jsImportsRegex = /\bjs.depends:([^\s]*)/;
+            if (query.match(jsImportsRegex)) {
+                const jsImportsFilter = query.match(jsImportsRegex);
+                const jsPkg =
+                    jsImportsFilter && jsImportsFilter.length >= 1
+                        ? jsImportsFilter[1]
+                        : "";
+                const es6Import = "^import.*'" + jsPkg + "[/?^\\s]*'";
+                const commonJSImport = "require\\('" + jsPkg + "[^\\s]*'\\)";
+                return query.replace(
+                    jsImportsRegex,
+                    `(${es6Import}|${commonJSImport})`
+                );
+            }
+            return query;
+        },
+    });
 
-    sourcegraph.workspace.onDidOpenTextDocument.subscribe(doc => {
-        if (doc.languageId !== 'typescript' && doc.languageId !== 'javascript') {
-            return
+    sourcegraph.workspace.openedTextDocuments.subscribe((doc) => {
+        if (
+            (doc.languageId !== "typescript" &&
+                doc.languageId !== "javascript") ||
+            !doc.text
+        ) {
+            return;
         }
-        from(doc.text.split('\n')).pipe(
-            concatMap(
-                (line, lineNumber) => {
+        from(doc.text.split("\n"))
+            .pipe(
+                concatMap((line, lineNumber) => {
                     // An issue with the second regex is that it'll match lines like
                     //  return "string"
-                    const jsPkgRegex = /^import\s.*\'([\w\/]*)[/?^\s]*\'|require\(\'([^\s]*)[^\s]*\'\)/
+                    const jsPkgRegex =
+                        /^import\s.*\'([\w\/]*)[/?^\s]*\'|require\(\'([^\s]*)[^\s]*\'\)/;
                     const match = jsPkgRegex.exec(line);
                     if (match && match.length > 1) {
                         // The match index depends on which regex pattern actually produced a match
-                        const pkgName = match[1] ? match[1] : match[2]
-                        return of({lineNumber, pkgName});
+                        const pkgName = match[1] ? match[1] : match[2];
+                        return of({ lineNumber, pkgName });
                     }
                     return EMPTY;
+                }),
+                toArray()
+            )
+            .subscribe((matches) => {
+                if (!matches) {
+                    return;
                 }
-            ),
-            toArray()
-        ).subscribe(matches => {
-            if (!matches) {
-                return
-            }
-            if (
-                sourcegraph.app.activeWindow &&
-                sourcegraph.app.activeWindow.visibleViewComponents.length >
-                    0
-            ) {
-                sourcegraph.app.activeWindow.visibleViewComponents[0].setDecorations(
-                    null,
-                    matches.map(match => ({
+                if (
+                    sourcegraph.app.activeWindow?.activeViewComponent?.type ===
+                    "CodeEditor"
+                ) {
+                    sourcegraph.app.activeWindow.activeViewComponent.setDecorations(
+                        jsDependencySearchDecorationType,
+                        matches.map((match) => ({
                             range: new sourcegraph.Range(
                                 new sourcegraph.Position(match.lineNumber, 0),
                                 new sourcegraph.Position(match.lineNumber, 0)
                             ),
                             after: {
-                                contentText: 'See all usages',
-                                linkURL: '/search?q=js.depends:' + match.pkgName,
-                                backgroundColor: 'pink',
-                                color: 'black'
-                            }
-                    })
-                ))
-            }
+                                contentText: "See all usages",
+                                linkURL:
+                                    "/search?q=js.depends:" + match.pkgName,
+                                backgroundColor: "pink",
+                                color: "black",
+                            },
+                        }))
+                    );
+                }
             });
     });
-}
-
 }
 
 // See https://about.sourcegraph.com/blog/extension-authoring for instructions and examples.
